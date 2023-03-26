c8d40204c337fe21f29e3627f740a3f09716de09
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-03-26 00:43:12 -0300

    fix(macos): launch
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- darwin.nix
+++ darwin.nix
@@ -1,10 +1,44 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ pinentry_mac ];
 
-  home.file."Library/LaunchAgents/pbcopy.plist" = {
-    text = builtins.readFile ./darwin/pbcopy.plist;
-  };
-  home.file."Library/LaunchAgents/pbpaste.plist" = {
-    text = builtins.readFile ./darwin/pbpaste.plist;
+  launchd.agents = {
+    pbcopy = {
+      enable = true;
+      config = {
+        Label = "localhost.pbcopy";
+        ProgramArguments = [ "/usr/bin/pbcopy" ];
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        ProcessType = "Background";
+        inetdCompatibility = { Wait = false; };
+        Sockets = {
+          Listener = {
+            SockServiceName = "2224";
+            SockNodeName = "127.0.0.1";
+          };
+        };
+      };
+    };
+    pbpaste = {
+      enable = true;
+      config = {
+        Label = "localhost.pbpaste";
+        ProgramArguments = [ "/usr/bin/pbpaste" ];
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        ProcessType = "Background";
+        inetdCompatibility = { Wait = false; };
+        Sockets = {
+          Listener = {
+            SockServiceName = "2225";
+            SockNodeName = "127.0.0.1";
+          };
+        };
+      };
+    };
   };
 }
