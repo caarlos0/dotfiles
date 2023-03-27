9255cf41fba0d4890d70889abf17481bb8535e3d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-03-27 15:37:25 -0300

    feat: darkstar
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -46,5 +46,23 @@
             nix-index-database.hmModules.nix-index
           ];
         };
+      homeConfigurations."carlos@darkstar" =
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs system; };
+          modules = [
+            ./modules/home.nix
+            ./modules/wezterm/default.nix
+            ./modules/tmux/default.nix
+            ./modules/dev/default.nix
+            ./modules/gpg.nix
+            ./modules/git.nix
+            ./modules/shell.nix
+            ./modules/ssh.nix
+            ./modules/bins/default.nix
+            ./modules/charm.nix
+            nix-index-database.hmModules.nix-index
+          ];
+        };
     };
 }
