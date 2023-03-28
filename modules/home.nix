84abf3239e1381037f81828536c5de19b0554cde
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-03-28 15:14:39 -0300

    chore: organize
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- modules/home.nix
+++ modules/home.nix
@@ -10,7 +10,6 @@
     cosign
     curl
     fd
-    fzf
     gh
     go-task
     htmltest
@@ -83,19 +82,27 @@
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
-  programs.fzf.colors = {
-    "bg+" = "#1e1e2e";
-    "fg+" = "#cdd6f4";
-    "hl+" = "#f38ba8";
-    bg = "#000000";
-    fg = "#cdd6f4";
-    header = "#f38ba8";
-    hl = "#f38ba8";
-    info = "#cba6f7";
-    marker = "#f5e0dc";
-    pointer = "#f5e0dc";
-    prompt = "#cba6f7";
-    spinner = "#f5e0dc";
+  programs = {
+    fzf = {
+      enable = true;
+      colors = {
+        "bg+" = "#1e1e2e";
+        "fg+" = "#cdd6f4";
+        "hl+" = "#f38ba8";
+        bg = "#000000";
+        fg = "#cdd6f4";
+        header = "#f38ba8";
+        hl = "#f38ba8";
+        info = "#cba6f7";
+        marker = "#f5e0dc";
+        pointer = "#f5e0dc";
+        prompt = "#cba6f7";
+        spinner = "#f5e0dc";
+      };
+    };
+
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
   };
 
   # You can also manage environment variables but you will have to manually
@@ -113,9 +120,6 @@
     LC_CTYPE = "en_US.UTF-8";
     PROJECTS = "$HOME/Developer";
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 
   # imports = [] ++ (optionals isLinux [./linux.nix]);
   # imports = [ ] ++ (optionals isDarwin [ ./darwin.nix ]);
