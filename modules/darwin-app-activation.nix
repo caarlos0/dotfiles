57163f26e816851d7be000f2150d958b3316c516
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-04-02 19:04:40 +0000

    fix(nix): actually copy the app over on darwin activation
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- modules/darwin-app-activation.nix
+++ modules/darwin-app-activation.nix
@@ -1,9 +1,5 @@
 # courtesy of https://github.com/kamalmarhubi/dotfiles-nix/blob/f8f5ad4f899b07beb881e4391bd60425f6ae2de0/darwin.nix
-{ config, inputs, lib, pkgs, ... }:
-let
-  system = "aarch64-darwin";
-  mkalias = inputs.mkAlias.outputs.apps.${system}.default.program;
-in {
+{ config, lib, pkgs, ... }: {
   disabledModules = [ "targets/darwin/linkapps.nix" ];
   # Add a custom mkalias based thing cribbed from:
   #   https://github.com/nix-community/home-manager/issues/1341#issuecomment-1446696577
@@ -22,11 +18,8 @@ in {
     in lib.hm.dag.entryAfter [ "linkGeneration" ] ''
       app_path="$HOME/Applications/Home Manager Apps"
       echo "Linking Home Manager applications..." 2>&1
-      tmp_path="$(mktemp -dt "home-manager-applications.XXXXXXXXXX")" || exit 1
       ${pkgs.fd}/bin/fd \
           -t l -d 1 . ${apps}/Applications \
-          -x $DRY_RUN_CMD ${mkalias} -L {} "$tmp_path/{/}"
-      $DRY_RUN_CMD rm -rf "$app_path"
-      $DRY_RUN_CMD mv "$tmp_path" "$app_path"
+          -x $DRY_RUN_CMD cp -f {} "app_path/{/}"
     '');
 }
