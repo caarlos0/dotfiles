67056c6ab79aa6e1bf6c38517970e303e8423242
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-04-03 09:35:14 -0300

    feat(nvim): improve fugitive keybindings

--- modules/dev/neovim/lua/user/fugitive.lua
+++ modules/dev/neovim/lua/user/fugitive.lua
@@ -6,14 +6,26 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
       return
     end
 
-    vim.keymap.set("n", "gPp", function()
+    local keymap = function(lhs, rhs, desc)
+      vim.keymap.set("n", lhs, rhs, {
+        noremap = true,
+        silent = true,
+        buffer = vim.api.nvim_get_current_buf(),
+        desc = desc,
+      })
+    end
+
+    keymap("<leader>p", function()
       vim.cmd.Git("push")
-    end, {
-      buffer = vim.api.nvim_get_current_buf(),
-      noremap = true,
-      silent = true,
-      desc = "Git push",
-    })
+    end, "Git push")
+
+    keymap("<leader>P", function()
+      vim.cmd.Git({ "pull", "--rebase" })
+    end, "Git pull --rebase")
+
+    keymap("<leader>t", function()
+      vim.cmd.Git({ "push", "-u", "origin" })
+    end, "Git push tracking origin")
   end,
 })
 
