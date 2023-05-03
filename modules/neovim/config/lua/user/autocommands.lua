9ca8680c78d5d1a3604b44970f83ea78961ecd12
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-03 02:31:39 +0000

    feat(nvim): open help to the right

--- modules/neovim/config/lua/user/autocommands.lua
+++ modules/neovim/config/lua/user/autocommands.lua
@@ -62,3 +62,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.b.miniindentscope_disable = true
   end,
 })
+
+-- Open help window in a vertical split to the right.
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("help_window_right", {}),
+  pattern = { "*.txt" },
+  callback = function()
+    if vim.o.filetype == "help" then
+      vim.cmd.wincmd("L")
+    end
+  end,
+})
