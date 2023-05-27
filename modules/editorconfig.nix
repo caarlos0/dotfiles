39d059aac29a654713ad8e497b17c6a8b5ccd413
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-27 03:44:48 +0000

    fix(core): editorconfig

--- modules/editorconfig.nix
+++ modules/editorconfig.nix
@@ -8,12 +8,16 @@
         trim_trailing_whitespace = true;
         insert_final_newline = true;
         max_line_width = 78;
+        indent_style = "space";
+        indent_size = 2;
+      };
+      "*.go" = {
         indent_style = "tab";
         indent_size = 4;
       };
-      "*.{rb,yaml,yml,lua,nix}" = {
-        indent_style = "space";
-        indent_size = 2;
+      "Makefile" = {
+        indent_style = "tab";
+        indent_size = 4;
       };
     };
   };
