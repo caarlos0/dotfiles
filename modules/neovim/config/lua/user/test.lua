d8ee42c7f59ca48ae59819d2ca59822eb87278b9
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-07-17 02:43:02 +0000

    fix(nvim): test improvements

--- modules/neovim/config/lua/user/test.lua
+++ modules/neovim/config/lua/user/test.lua
@@ -64,18 +64,9 @@ end, {
   desc = "Jump to next failed test",
 })
 
-local neotest_ns = vim.api.nvim_create_namespace("neotest")
-vim.diagnostic.config({
-  virtual_text = {
-    format = function(diagnostic)
-      local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-      return message
-    end,
-  },
-}, neotest_ns)
-
 neotest.setup({
   diagnostic = { enabled = false },
+  quickfix = { open = false },
   adapters = {
     require("neotest-jest"),
     require("neotest-rust"),
