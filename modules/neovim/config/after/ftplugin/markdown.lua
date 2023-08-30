21e535f3108ae3a488e723962367304f1588fcc5
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-08-30 09:11:42 -0300

    feat(nvim): leader-op to toggle markdown checkboxes

--- modules/neovim/config/after/ftplugin/markdown.lua
+++ modules/neovim/config/after/ftplugin/markdown.lua
@@ -10,3 +10,22 @@ if require("zk.util").notebook_root(vim.fn.expand("%:p")) ~= nil then
 
   -- TODO: add keymaps?
 end
+
+local bufnr = vim.api.nvim_buf_get_number(0)
+vim.keymap.set("n", "<leader>op", function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local lineno = cursor[1]
+  local line = vim.api.nvim_buf_get_lines(bufnr, lineno - 1, lineno, false)[1] or ""
+  if string.find(line, "%[ %]") then
+    line = line:gsub("%[ %]", "%[x%]")
+  else
+    line = line:gsub("%[x%]", "%[ %]")
+  end
+  vim.api.nvim_buf_set_lines(bufnr, lineno - 1, lineno, false, { line })
+  vim.api.nvim_win_set_cursor(0, cursor)
+end, {
+  noremap = true,
+  silent = true,
+  desc = "Toggle checkbox",
+  buffer = bufnr,
+})
