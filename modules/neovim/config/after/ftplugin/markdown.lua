ab1fa3648a61982af35055062cda37dd936fe5b3
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-09-06 13:56:31 -0300

    fix(nvim): improve leader-op

--- modules/neovim/config/after/ftplugin/markdown.lua
+++ modules/neovim/config/after/ftplugin/markdown.lua
@@ -7,25 +7,22 @@ if require("zk.util").notebook_root(vim.fn.expand("%:p")) ~= nil then
     require("user.zk")
     vim.g.loaded_zk = 1
   end
-
-  -- TODO: add keymaps?
 end
 
-local bufnr = vim.api.nvim_buf_get_number(0)
 vim.keymap.set("n", "<leader>op", function()
   local cursor = vim.api.nvim_win_get_cursor(0)
   local lineno = cursor[1]
-  local line = vim.api.nvim_buf_get_lines(bufnr, lineno - 1, lineno, false)[1] or ""
+  local line = vim.api.nvim_buf_get_lines(0, lineno - 1, lineno, false)[1] or ""
   if string.find(line, "%[ %]") then
     line = line:gsub("%[ %]", "%[x%]")
   else
     line = line:gsub("%[x%]", "%[ %]")
   end
-  vim.api.nvim_buf_set_lines(bufnr, lineno - 1, lineno, false, { line })
+  vim.api.nvim_buf_set_lines(0, lineno - 1, lineno, false, { line })
   vim.api.nvim_win_set_cursor(0, cursor)
 end, {
   noremap = true,
   silent = true,
   desc = "Toggle checkbox",
-  buffer = bufnr,
+  buffer = 0,
 })
