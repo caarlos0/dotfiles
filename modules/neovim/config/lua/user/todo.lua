f31fbca02c783c108f74db20a00f4f7ec3ff0d10
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-09-22 02:13:22 +0000

    fix(nvim): :intro flashing when opening nvim
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- modules/neovim/config/lua/user/todo.lua
+++ modules/neovim/config/lua/user/todo.lua
@@ -1,8 +1,19 @@
 local todo = require("todo-comments")
-todo.setup({
-  highlight = {
-    keyword = "bg",
-  },
+
+-- todo-comments create new highlight groups on setup, which causes a redraw,
+-- which will steal the :intro screen, which I like to keep.
+--
+-- This will also lazy load it, so that's also good :)
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  once = true,
+  callback = function()
+    todo.setup({
+      highlight = {
+        keyword = "bg",
+      },
+    })
+  end,
 })
 
 vim.keymap.set("n", "]t", todo.jump_next, { desc = "Next todo comment" })
