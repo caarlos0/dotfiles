859e42aa9a91c88e3ab97b83b8df182a9bb9450b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-09-22 13:42:04 -0300

    fix(fish): catppuccin theme
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -93,4 +93,11 @@
   xdg.configFile."fish/functions" = {
     source = config.lib.file.mkOutOfStoreSymlink ./functions;
   };
+
+  xdg.configFile."fish/themes/Catppuccin Mocha.theme" = {
+    source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/fish/main/themes/Catppuccin%20Mocha.theme";
+      sha256 = "MlI9Bg4z6uGWnuKQcZoSxPEsat9vfi5O1NkeYFaEb2I=";
+    };
+  };
 }
