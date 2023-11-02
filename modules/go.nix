4045c0c428c8386a7367970a53632c8c7be93534
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-11-02 16:37:10 +0000

    fix: go 1.21 is default nixpkgs now

--- modules/go.nix
+++ modules/go.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.go = {
     enable = true;
-    package = pkgs.go_1_21;
+    package = pkgs.go;
     goPath = "Developer/Go";
     goPrivate = [
       "github.com/caarlos0"
