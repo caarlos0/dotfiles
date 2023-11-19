ba1d0d12cf292f23507c126f5593e3e78ce2983a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-11-19 14:46:36 -0300

    feat(nixos): darkstar needs gcc for CGO
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- machines/darkstar.nix
+++ machines/darkstar.nix
@@ -23,6 +23,11 @@
     '';
   };
 
+  environment.systemPackages = with pkgs; [
+    gcc
+  ];
+
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
