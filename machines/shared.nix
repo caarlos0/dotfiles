196323968c88a1d75353a375963f2762e12433c7
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-11-27 22:21:41 -0300

    feat(nixos): use cachix

--- machines/shared.nix
+++ machines/shared.nix
@@ -80,6 +80,14 @@
     '';
     settings = {
       auto-optimise-store = true;
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       trusted-users = [
         "root"
         "carlos"
