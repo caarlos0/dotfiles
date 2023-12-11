d706f767afac47681bc3b4c4f43c697acb13631a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-12-11 09:15:50 -0300

    fix: homepage

--- machines/media.nix
+++ machines/media.nix
@@ -3,6 +3,33 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { pkgs, ... }:
+let
+  homepage-bookmarks = pkgs.writeTextFile {
+    name = "bookmarks.yaml";
+    executable = false;
+    destination = "/var/lib/private/homepage-dashboard/bookmarks.yaml";
+    text = ''
+      ---
+      # For configuration options and examples, please see:
+      # https://gethomepage.dev/en/configs/bookmarks
+
+      - Developer:
+          - Github:
+              - abbr: GH
+                href: https://github.com/
+
+      - Social:
+          - Twitter:
+              - abbr: X
+                href: https://twitter.com/
+
+      - Entertainment:
+          - YouTube:
+              - abbr: YT
+                href: https://youtube.com/
+    '';
+  };
+in
 {
   imports =
     [
@@ -12,6 +39,12 @@
 
   networking.hostName = "media";
   services.qemuGuest.enable = true;
+
+  services.homepage-dashboard = {
+    enable = true;
+    package = pkgs.unstable.homepage-dashboard;
+    openFirewall = true;
+  };
 
   services.nginx = {
     enable = true;
