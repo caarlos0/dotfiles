1b4cd3cb43f07690317bd7c2ee408637c4592a91
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-12-11 23:47:56 -0300

    feat(nixos): run binary cache on cachixe

--- machines/cachixe.nix
+++ machines/cachixe.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ ... }:
+{ config, ... }:
 {
   imports =
     [
@@ -12,6 +12,20 @@
   networking.hostName = "cachixe";
   services.qemuGuest.enable = true;
 
+  services.nix-serve = {
+    enable = true;
+    secretKeyFile = "/var/cache/cache-priv-key.pem";
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      "cachixe.local" = {
+        locations."/".proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
+      };
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
