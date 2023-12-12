c38b5f903a78472764fd0a494abac5e16d78ca26
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-12-11 23:44:01 -0300

    feat(nixos): cachixe

---
+++ machines/cachixe.nix
@@ -0,0 +1,24 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ ... }:
+{
+  imports =
+    [
+      ./shared.nix
+      ./hardware/cachixe.nix
+    ];
+
+  networking.hostName = "cachixe";
+  services.qemuGuest.enable = true;
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
+
