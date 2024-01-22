98547eb8a68b5dd9363e9c05f4f9cc42db6f0675
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-01-21 22:01:31 -0300

    fix(darwin): fix launchd pbcopy/pbpaste
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- machines/supernova/default.nix
+++ machines/supernova/default.nix
@@ -109,47 +109,4 @@
       };
     };
   };
-
-  launchd.agents = {
-    pbcopy = {
-      serviceConfig = {
-        Label = "localhost.pbcopy";
-        ProgramArguments = [ "/usr/bin/pbcopy" ];
-        RunAtLoad = true;
-        KeepAlive = {
-          Crashed = true;
-          SuccessfulExit = false;
-        };
-        ProcessType = "Background";
-        EnvironmentVariables = { "LC_CTYPE" = "UTF-8"; };
-        inetdCompatibility = { Wait = false; };
-        Sockets = {
-          Listener = {
-            SockServiceName = "2224";
-            SockNodeName = "127.0.0.1";
-          };
-        };
-      };
-    };
-    pbpaste = {
-      serviceConfig = {
-        Label = "localhost.pbpaste";
-        ProgramArguments = [ "/usr/bin/pbpaste" ];
-        RunAtLoad = true;
-        KeepAlive = {
-          Crashed = true;
-          SuccessfulExit = false;
-        };
-        ProcessType = "Background";
-        inetdCompatibility = { Wait = false; };
-        EnvironmentVariables = { "LC_CTYPE" = "UTF-8"; };
-        Sockets = {
-          Listener = {
-            SockServiceName = "2225";
-            SockNodeName = "127.0.0.1";
-          };
-        };
-      };
-    };
-  };
 }
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -20,4 +20,50 @@
     fish_add_path -a /Applications/Postgres.app/Contents/Versions/latest/bin/
     fish_add_path -a /opt/homebrew/bin/
   '';
+
+
+  launchd.agents = {
+    pbcopy = {
+      enable = true;
+      config = {
+        Label = "localhost.pbcopy";
+        ProgramArguments = [ "/usr/bin/pbcopy" ];
+        RunAtLoad = true;
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        ProcessType = "Background";
+        EnvironmentVariables = { "LC_CTYPE" = "UTF-8"; };
+        inetdCompatibility = { Wait = false; };
+        Sockets = {
+          Listener = {
+            SockServiceName = "2224";
+            SockNodeName = "127.0.0.1";
+          };
+        };
+      };
+    };
+    pbpaste = {
+      enable = true;
+      config = {
+        Label = "localhost.pbpaste";
+        ProgramArguments = [ "/usr/bin/pbpaste" ];
+        RunAtLoad = true;
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        ProcessType = "Background";
+        inetdCompatibility = { Wait = false; };
+        EnvironmentVariables = { "LC_CTYPE" = "UTF-8"; };
+        Sockets = {
+          Listener = {
+            SockServiceName = "2225";
+            SockNodeName = "127.0.0.1";
+          };
+        };
+      };
+    };
+  };
 }
