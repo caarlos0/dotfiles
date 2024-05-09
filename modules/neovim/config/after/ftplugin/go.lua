fb2cc9096f80a26068e93280ef49be2a99736ab8
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-05-09 16:26:16 -0300

    fix(nvim): improve gomod

--- modules/neovim/config/after/ftplugin/go.lua
+++ modules/neovim/config/after/ftplugin/go.lua
@@ -7,7 +7,7 @@ vim.api.nvim_buf_create_user_command(bufnr, "GoModTidy", function()
   vim.notify("Running `go mod tidy`...")
   local uri = vim.uri_from_bufnr(bufnr)
   local arguments = { { URIs = { uri } } }
-  vim.cmd.wall()
+  vim.cmd([[ noautocmd wall ]])
   vim.lsp.buf.execute_command({
     command = "gopls.tidy",
     arguments = arguments,
--- modules/neovim/config/after/ftplugin/gomod.lua
+++ modules/neovim/config/after/ftplugin/gomod.lua
@@ -1,11 +1,2 @@
 vim.cmd.runtime({ "after/ftplugin/go.lua", bang = true })
 vim.cmd.runtime({ "lua/autoread.lua", bang = true })
-
-local bufnr = vim.api.nvim_get_current_buf()
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("GoMod", { clear = true }),
-  buffer = bufnr,
-  callback = function()
-    vim.cmd.GoModTidy()
-  end,
-})
