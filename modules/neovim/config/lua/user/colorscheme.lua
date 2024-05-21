a91cafc239812f9b7f9f71d530abd4a8c05a6553
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-05-20 22:48:38 -0300

    feat(nvim): contrast linenr, improve cmd menu

--- modules/neovim/config/lua/user/colorscheme.lua
+++ modules/neovim/config/lua/user/colorscheme.lua
@@ -1,4 +1,37 @@
 require("catppuccin").setup({
+  highlight_overrides = {
+    all = function(colors)
+      return {
+        LineNr = { fg = colors.overlay0 },
+        CmpItemKindSnippet = { fg = colors.base, bg = colors.mauve },
+        CmpItemKindKeyword = { fg = colors.base, bg = colors.red },
+        CmpItemKindText = { fg = colors.base, bg = colors.teal },
+        CmpItemKindMethod = { fg = colors.base, bg = colors.blue },
+        CmpItemKindConstructor = { fg = colors.base, bg = colors.blue },
+        CmpItemKindFunction = { fg = colors.base, bg = colors.blue },
+        CmpItemKindFolder = { fg = colors.base, bg = colors.blue },
+        CmpItemKindModule = { fg = colors.base, bg = colors.blue },
+        CmpItemKindConstant = { fg = colors.base, bg = colors.peach },
+        CmpItemKindField = { fg = colors.base, bg = colors.green },
+        CmpItemKindProperty = { fg = colors.base, bg = colors.green },
+        CmpItemKindEnum = { fg = colors.base, bg = colors.green },
+        CmpItemKindUnit = { fg = colors.base, bg = colors.green },
+        CmpItemKindClass = { fg = colors.base, bg = colors.yellow },
+        CmpItemKindVariable = { fg = colors.base, bg = colors.flamingo },
+        CmpItemKindFile = { fg = colors.base, bg = colors.blue },
+        CmpItemKindInterface = { fg = colors.base, bg = colors.yellow },
+        CmpItemKindColor = { fg = colors.base, bg = colors.red },
+        CmpItemKindReference = { fg = colors.base, bg = colors.red },
+        CmpItemKindEnumMember = { fg = colors.base, bg = colors.red },
+        CmpItemKindStruct = { fg = colors.base, bg = colors.blue },
+        CmpItemKindValue = { fg = colors.base, bg = colors.peach },
+        CmpItemKindEvent = { fg = colors.base, bg = colors.blue },
+        CmpItemKindOperator = { fg = colors.base, bg = colors.blue },
+        CmpItemKindTypeParameter = { fg = colors.base, bg = colors.blue },
+        CmpItemKindCopilot = { fg = colors.base, bg = colors.teal },
+      }
+    end,
+  },
   flavour = "mocha",
   integrations = {
     cmp = true,
