e2533acef1c98eb4e2d97cb0081205e734b4eeba
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-07-23 12:12:13 -0400

    fix(nix): index database
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- modules/home.nix
+++ modules/home.nix
@@ -4,6 +4,7 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.nix-index.enable = true;
 
   home.sessionVariables = {
     LC_ALL = "en_US.UTF-8";
