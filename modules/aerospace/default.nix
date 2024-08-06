b73307ac4c6778fc8175eaae96fcfa73d0752152
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-05 22:49:08 -0300

    wip
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- modules/aerospace/default.nix
+++ modules/aerospace/default.nix
@@ -7,59 +7,10 @@ let
 in
 {
   home.file."${aerospaceConfigPath}".source = toml.generate "aerospace.toml" {
-    # Place a copy of this config to ~/.aerospace.toml
-    # After that, you can edit ~/.aerospace.toml to your liking
-
-    # It's not necessary to copy all keys to your config.
-    # If the key is missing in your config, "default-config.toml" will serve as a fallback
-
-    # You can use it to add commands that run after login to macOS user session.
-    # 'start-at-login' needs to be 'true' for 'after-login-command' to work
-    # Available commands: https://nikitabobko.github.io/AeroSpace/commands
-    after-login-command = [ ];
-
-    # You can use it to add commands that run after AeroSpace startup.
-    # 'after-startup-command' is run after 'after-login-command'
-    # Available commands : https://nikitabobko.github.io/AeroSpace/commands
-    after-startup-command = [ ];
-
-    # Start AeroSpace at login
     start-at-login = true;
-
-    # Normalizations. See: https://nikitabobko.github.io/AeroSpace/guide#normalization
-    enable-normalization-flatten-containers = true;
-    enable-normalization-opposite-orientation-for-nested-containers = true;
-
-    # See: https://nikitabobko.github.io/AeroSpace/guide#layouts
-    # The 'accordion-padding' specifies the size of accordion padding
-    # You can set 0 to disable the padding feature
-    accordion-padding = 30;
-
-    # Possible values: tiles|accordion
     default-root-container-layout = "tiles";
 
-    # Possible values: horizontal|vertical|auto
-    # 'auto' means: wide monitor (anything wider than high) gets horizontal orientation,
-    #               tall monitor (anything higher than wide) gets vertical orientation
-    default-root-container-orientation = "auto";
-
-    # Possible values: (qwerty|dvorak)
-    # See https://nikitabobko.github.io/AeroSpace/guide#key-mapping
-    key-mapping.preset = "qwerty";
-
-    # Mouse follows focus when focused monitor changes
-    # Drop it from your config, if you don't like this behavior
-    # See https://nikitabobko.github.io/AeroSpace/guide#on-focus-changed-callbacks
-    # See https://nikitabobko.github.io/AeroSpace/commands#move-mouse
-    on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
-
-    # Gaps between windows (inner-*) and between monitor edges (outer-*).
-    # Possible values:
-    # - Constant:     gaps.outer.top = 8
-    # - Per monitor:  gaps.outer.top = [{ monitor.main = 16 }, { monitor."some-pattern" = 32 }, 24]
-    #                 In this example, 24 is a default value when there is no match.
-    #                 Monitor pattern is the same as for 'workspace-to-monitor-force-assignment'.
-    #                 See: https://nikitabobko.github.io/AeroSpace/guide#assign-workspaces-to-monitors
+    accordion-padding = 0;
     gaps = {
       inner.horizontal = 0;
       inner.vertical = 0;
@@ -69,79 +20,46 @@ in
       outer.right = 0;
     };
 
-    # 'main' binding mode declaration
-    # See: https://nikitabobko.github.io/AeroSpace/guide#binding-modes
-    # 'main' binding mode must be always presented
     mode.main.binding = {
-      # All possible keys:
-      # - Letters.        a, b, c, ..., z
-      # - Numbers.        0, 1, 2, ..., 9
-      # - Keypad numbers. keypad0, keypad1, keypad2, ..., keypad9
-      # - F-keys.         f1, f2, ..., f20
-      # - Special keys.   minus, equal, period, comma, slash, backslash, quote, semicolon, backtick,
-      #                   leftSquareBracket, rightSquareBracket, space, enter, esc, backspace, tab
-      # - Keypad special. keypadClear, keypadDecimalMark, keypadDivide, keypadEnter, keypadEqual,
-      #                   keypadMinus, keypadMultiply, keypadPlus
-      # - Arrows.         left, down, up, right
+      ctrl-alt-cmd-shift-y = open "Music.app";
+      ctrl-alt-cmd-shift-u = open "Ghostty.app";
+      ctrl-alt-cmd-shift-i = open "Safari.app";
+      ctrl-alt-cmd-shift-o = open "Notes.app";
+      ctrl-alt-cmd-shift-p = open "Discord.app";
 
-      # All possible modifiers: cmd, alt, ctrl, shift
-
-      # All possible commands: https://nikitabobko.github.io/AeroSpace/commands
-
-      # You can uncomment this line to open up terminal with alt + enter shortcut
-      # See: https://nikitabobko.github.io/AeroSpace/commands#exec-and-forget
-      # alt-enter = 'exec-and-forget open -n /System/Applications/Utilities/Terminal.app'
-
-      alt-u = open "Ghostty.app";
-      alt-i = open "Safari.app";
-      alt-o = open "Notes.app";
-      alt-p = open "Discord.app";
-
-      # See: https://nikitabobko.github.io/AeroSpace/commands#layout
       alt-slash = "layout tiles horizontal vertical";
       alt-comma = "layout accordion horizontal vertical";
 
-      # See: https://nikitabobko.github.io/AeroSpace/commands#focus
       alt-h = "focus left";
       alt-j = "focus down";
       alt-k = "focus up";
       alt-l = "focus right";
 
-      # See: https://nikitabobko.github.io/AeroSpace/commands#move
       alt-shift-h = "move left";
       alt-shift-j = "move down";
       alt-shift-k = "move up";
       alt-shift-l = "move right";
 
-      # See: https://nikitabobko.github.io/AeroSpace/commands#resize
       alt-shift-minus = "resize smart -50";
       alt-shift-equal = "resize smart +50";
 
-      # See: https://nikitabobko.github.io/AeroSpace/commands#workspace
       alt-1 = "workspace 1";
       alt-2 = "workspace 2";
       alt-3 = "workspace 3";
       alt-4 = "workspace 4";
       alt-5 = "workspace 5";
 
-      # See: https://nikitabobko.github.io/AeroSpace/commands#move-node-to-workspace
       alt-shift-1 = "move-node-to-workspace 1";
       alt-shift-2 = "move-node-to-workspace 2";
       alt-shift-3 = "move-node-to-workspace 3";
       alt-shift-4 = "move-node-to-workspace 4";
       alt-shift-5 = "move-node-to-workspace 5";
 
-      # See: https://nikitabobko.github.io/AeroSpace/commands#workspace-back-and-forth
       alt-tab = "workspace-back-and-forth";
-      # See: https://nikitabobko.github.io/AeroSpace/commands#move-workspace-to-monitor
       alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
-
-      # See: https://nikitabobko.github.io/AeroSpace/commands#mode
       alt-shift-semicolon = "mode service";
     };
 
-    # 'service' binding mode declaration.
-    # See: https://nikitabobko.github.io/AeroSpace/guide#binding-modes
     mode.service.binding = {
       esc = [ "reload-config" "mode main" ];
       r = [ "flatten-workspace-tree" "mode main" ]; # reset layout
