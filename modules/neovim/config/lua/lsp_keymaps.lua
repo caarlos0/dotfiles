cdf8503160b0b91e36809997792059ee1e95f4fd
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-18 22:41:48 -0300

    fix(nvim): equal keymaps on nvim v0.10 and v0.11

--- modules/neovim/config/lua/lsp_keymaps.lua
+++ modules/neovim/config/lua/lsp_keymaps.lua
@@ -12,37 +12,54 @@ local keymap = function(lhs, rhs, bufnr)
   })
 end
 
+local is_v10 = vim.fn.has("nvim-0.10") == 1
+
 --- On attach for key maps.
 ---@param bufnr number Buffer number
 M.on_attach = function(bufnr)
   local builtin = require("telescope.builtin")
-  keymap("gd", builtin.lsp_definitions, bufnr)
+
+  -- deprecated keymaps
   keymap("gr", function()
     vim.notify("use grr now")
   end, bufnr)
-  keymap("<leader>ls", builtin.lsp_document_symbols, bufnr)
-  keymap("<leader>lS", builtin.lsp_dynamic_workspace_symbols, bufnr)
   keymap("gs", function()
-    vim.notify("use <ctrl>+s now")
+    vim.notify("use <C+s> now")
   end, bufnr)
-  keymap("gi", builtin.lsp_implementations, bufnr)
-  keymap("gD", vim.lsp.buf.declaration, bufnr)
-  keymap("K", vim.lsp.buf.hover, bufnr)
-  keymap("<leader>D", builtin.lsp_type_definitions, bufnr)
-  keymap("<leader>cl", vim.lsp.codelens.run, bufnr)
   keymap("<leader>rn", function()
     vim.notify("use grn now")
   end, bufnr)
   keymap("<leader>ca", function()
     vim.notify("use gra now")
   end, bufnr)
+
+  keymap("gd", builtin.lsp_definitions, bufnr)
+  keymap("grr", builtin.lsp_references, bufnr)
+  keymap("<leader>ls", builtin.lsp_document_symbols, bufnr)
+  keymap("<leader>lS", builtin.lsp_dynamic_workspace_symbols, bufnr)
+  keymap("<C+s>", vim.lsp.buf.signature_help, bufnr)
+  keymap("gi", builtin.lsp_implementations, bufnr)
+  keymap("gD", vim.lsp.buf.declaration, bufnr)
+  keymap("K", vim.lsp.buf.hover, bufnr)
+  keymap("<leader>D", builtin.lsp_type_definitions, bufnr)
+  keymap("<leader>cl", vim.lsp.codelens.run, bufnr)
+  keymap("grn", vim.lsp.buf.rename, bufnr)
+  keymap("gra", vim.lsp.buf.code_action, bufnr)
   keymap("<leader>gl", vim.diagnostic.open_float, bufnr)
   keymap("[d", function()
-    vim.diagnostic.jump({ count = -1 })
+    if is_v10 then
+      vim.diagnostic.goto_prev()
+    else
+      vim.diagnostic.jump({ count = -1 })
+    end
     vim.cmd("norm zz")
   end, bufnr)
   keymap("]d", function()
-    vim.diagnostic.jump({ count = 1 })
+    if is_v10 then
+      vim.diagnostic.goto_next()
+    else
+      vim.diagnostic.jump({ count = 1 })
+    end
     vim.cmd("norm zz")
   end, bufnr)
 
