9c65e7c814f14c7aa5f05680f936188023c1e56c
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-10-01 16:34:03 -0300

    feat(nvim): move gomodtidy to <F4>

--- modules/neovim/config/after/ftplugin/go.lua
+++ modules/neovim/config/after/ftplugin/go.lua
@@ -3,6 +3,17 @@ local bufnr = vim.api.nvim_get_current_buf()
 
 vim.opt_local.formatoptions:append("jo")
 
+--- Add a normal keymap.
+---@param lhs string Keymap
+---@param rhs function Action
+local keymap = function(lhs, rhs)
+  vim.keymap.set("n", lhs, rhs, {
+    noremap = true,
+    silent = true,
+    buffer = bufnr,
+  })
+end
+
 vim.api.nvim_buf_create_user_command(bufnr, "GoModTidy", function()
   vim.notify("Running `go mod tidy`...")
   local uri = vim.uri_from_bufnr(bufnr)
@@ -14,9 +25,4 @@ vim.api.nvim_buf_create_user_command(bufnr, "GoModTidy", function()
   })
 end, { desc = "go mod tidy" })
 
-vim.keymap.set("n", "<leader>gmt", vim.cmd.GoModTidy, {
-  noremap = true,
-  silent = true,
-  buffer = bufnr,
-  desc = "Run go mod tidy",
-})
+keymap("<F4>", vim.cmd.GoModTidy)
