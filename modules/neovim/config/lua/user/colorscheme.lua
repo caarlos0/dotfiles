a43966fdd29232e6ecca5276cfddd92335e42580
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-10-31 13:14:46 -0300

    fix(nvim): more color links

--- modules/neovim/config/lua/user/colorscheme.lua
+++ modules/neovim/config/lua/user/colorscheme.lua
@@ -8,6 +8,9 @@ require("gruvbox").setup({
     GruvboxYellowSign = { bg = bg0 },
     GruvboxGreenSign = { bg = bg0 },
     GruvboxAquaSign = { bg = bg0 },
+    GruvboxOrangeSign = { bg = bg0 },
+    GruvboxPurpleSign = { bg = bg0 },
+    GruvboxBlueSign = { bg = bg0 },
     Normal = { bg = bg0 },
   },
 })
