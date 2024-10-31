e1df8c59139c729e93f61ed5019d0c9a9b050c25
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-10-31 11:23:26 -0300

    fix: colors
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -9,21 +9,18 @@
     # Reasons unknown, but I think it's somehow related to babelfish.
     colors = {
       "bg" = "#1b1b1b";
-      "bg+" = "#665c54";
-      "border" = "#458588";
+      "bg+" = "#3c3836";
       "fg" = "#ebdbb2";
-      "gutter" = "#1b1b1b";
-      "header" = "#fabd2f";
-      "hl" = "#83a598";
-      "hl+" = "#83a598";
-      "info" = "#928374";
-      "marker" = "#fb4934";
-      "pointer" = "#fb4934";
-      "prompt" = "#83a598";
-      "query" = "#ebdbb2:regular";
-      "scrollbar" = "#458588";
-      "separator" = "#fabd2f";
-      "spinner" = "#fb4934";
+      "fg+" = "#ebdbb2";
+      "header" = "#fb4934";
+      "hl" = "#fb4934";
+      "hl+" = "#fb4934";
+      "info" = "#d3869b";
+      "marker" = "#83a598";
+      "pointer" = "#ebdbb2";
+      "prompt" = "#d3869b";
+      "selected-bg" = "#665c54";
+      "spinner" = "#ebdbb2";
     };
   };
 }
