a1d95bca5831a0b19ed4f7470bd4340760cc29a6
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-10-31 13:13:24 -0300

    fix(nvim): gruvbox bg on signs

--- modules/neovim/config/lua/user/colorscheme.lua
+++ modules/neovim/config/lua/user/colorscheme.lua
@@ -3,7 +3,11 @@ require("gruvbox").setup({
   contrast = "hard",
   overrides = {
     GruvboxBg0 = { fg = bg0 },
-    SignColumn = { link = "GruvboxBg0" },
+    SignColumn = { bg = bg0 },
+    GruvboxRedSign = { bg = bg0 },
+    GruvboxYellowSign = { bg = bg0 },
+    GruvboxGreenSign = { bg = bg0 },
+    GruvboxAquaSign = { bg = bg0 },
     Normal = { bg = bg0 },
   },
 })
