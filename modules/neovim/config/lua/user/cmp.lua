9deed94ae0a4b530549216673b3809666adefbe0
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-11-06 10:11:39 -0300

    fix(nvim): scrolling docs keymaps

--- modules/neovim/config/lua/user/cmp.lua
+++ modules/neovim/config/lua/user/cmp.lua
@@ -32,8 +32,8 @@ cmp.setup({
     keyword_length = 3,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<C-y>"] = cmp.mapping.confirm({
