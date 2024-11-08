28db1873a00d4ff740a722f785fcd9739e99a94d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-11-08 13:31:45 -0300

    feat(nvim): add go.mod and go.work mappings to other.nvim

--- modules/neovim/config/lua/user/other.lua
+++ modules/neovim/config/lua/user/other.lua
@@ -1,6 +1,26 @@
 require("other-nvim").setup({
   mappings = {
     "golang",
+    {
+      pattern = "go.mod$",
+      target = "go.sum",
+      context = "sum",
+    },
+    {
+      pattern = "go.sum$",
+      target = "go.mod",
+      context = "mod",
+    },
+    {
+      pattern = "go.work$",
+      target = "go.work.sum",
+      context = "sum",
+    },
+    {
+      pattern = "go.work.sum$",
+      target = "go.work",
+      context = "mod",
+    },
   },
 })
 
