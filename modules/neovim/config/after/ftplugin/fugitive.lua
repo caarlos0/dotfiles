19f7581b1fb0559a2fee0c5a82071fa37166f250
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-11-14 23:46:21 -0300

    fix(nvim): fugitive push async (#28)
    * fix(nvim): improvements
    
    * fix: using async.void to make sure it runs async
    
    * fix: yet another try
    
    * feat(git): make gpr also available under git-ppr
    
    * fix(nvim): use plenary.job instead
    
    * fix: forgot to delete this

--- modules/neovim/config/after/ftplugin/fugitive.lua
+++ modules/neovim/config/after/ftplugin/fugitive.lua
@@ -1,4 +1,4 @@
-local async = require("plenary.async")
+local Job = require("plenary.job")
 
 local opts = {
   noremap = true,
@@ -6,33 +6,40 @@ local opts = {
   buffer = vim.api.nvim_get_current_buf(),
 }
 
+-- Runs a git command async.
+--
+---@param args table: git command arguments.
+---@param success_msg string: notification message when the command succeeds.
+---@param error_msg string: notification message when the command fails.
+local function async_git(args, success_msg, error_msg)
+  Job:new({
+    command = "git",
+    args = args,
+    ---@diagnostic disable: unused-local
+    on_exit = function(j, status)
+      if status == 0 then
+        vim.notify(success_msg, vim.log.levels.INFO)
+      else
+        vim.notify(error_msg, vim.log.levels.ERROR)
+      end
+    end,
+  }):start()
+end
+
 -- goes directly to the first file and opens diff view
 vim.cmd("normal )=")
 
 vim.keymap.set("n", "gp", function()
-  async.run(function()
-    vim.cmd([[
-      silent! Git push --quiet
-    ]])
-  end)
-  vim.cmd([[ close ]])
+  vim.cmd("close")
+  async_git({ "push", "--quiet" }, "Pushed!", "Push failed!")
 end, opts)
 
 vim.keymap.set("n", "gP", function()
-  async.run(function()
-    vim.cmd([[
-      silent! Git pull --rebase
-    ]])
-  end)
-  vim.cmd([[ close ]])
+  vim.cmd("close")
+  async_git({ "pull", "--rebase" }, "Pulled!", "Pull failed!")
 end, opts)
 
 vim.keymap.set("n", "go", function()
-  async.run(function()
-    vim.cmd([[
-      silent! Git push origin HEAD --quiet
-      silent! Git pr
-    ]])
-  end)
-  vim.cmd([[ close ]])
+  vim.cmd("close")
+  async_git({ "ppr" }, "Pushed and opened PR URL!", "Failed to push or open PR")
 end, opts)
