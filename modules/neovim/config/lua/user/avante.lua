dda296d37cf77a6b505c659f94dc0a61cd7c564c
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-02-24 17:03:39 -0300

    fix(nvim): avante claude 3.7

--- modules/neovim/config/lua/user/avante.lua
+++ modules/neovim/config/lua/user/avante.lua
@@ -1,18 +1,23 @@
 require("avante_lib").load()
 require("avante").setup({
   hints = { enabled = false },
-  cursor_applying_provider = "groq",
-  behaviour = {
-    enable_cursor_planning_mode = true,
-  },
+  provider = "claude37",
+  -- cursor_applying_provider = "groq",
+  -- behaviour = {
+  --   enable_cursor_planning_mode = true,
+  -- },
   vendors = {
-    groq = {
-      __inherited_from = "openai",
-      api_key_name = "GROQ_API_KEY",
-      endpoint = "https://api.groq.com/openai/v1/",
-      model = "llama-3.3-70b-versatile",
-      max_tokens = 32768,
+    claude37 = {
+      __inherited_from = "claude",
+      model = "claude-3-7-sonnet-20250219",
     },
+    -- groq = {
+    --   __inherited_from = "openai",
+    --   api_key_name = "GROQ_API_KEY",
+    --   endpoint = "https://api.groq.com/openai/v1/",
+    --   model = "llama-3.3-70b-versatile",
+    --   max_tokens = 32768,
+    -- },
   },
 })
 
