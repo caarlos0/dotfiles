5bc99482c3e9b074c22c3690adf4b7c692107571
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-03-19 09:56:19 -0300

    fix(go): remove goprivate

--- modules/go.nix
+++ modules/go.nix
@@ -7,10 +7,5 @@
     enable = true;
     package = pkgs.go_1_22;
     goPath = "Developer/Go";
-    goPrivate = [
-      "github.com/caarlos0"
-      "github.com/charmbracelet"
-      "github.com/goreleaser"
-    ];
   };
 }
