87463ff9800de5232294510730c37e67f89ff75b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-04-14 11:46:06 -0300

    fix(nvim): cmp config

--- modules/neovim/config/lua/user/cmp.lua
+++ modules/neovim/config/lua/user/cmp.lua
@@ -8,7 +8,7 @@ require("copilot").setup({
 require("blink.cmp").setup({
   keymap = { preset = "default" },
   appearance = {
-    use_nvim_cmp_as_default = true,
+    -- use_nvim_cmp_as_default = true,
     nerd_font_variant = "mono",
     kind_icons = {
       Array = "",
