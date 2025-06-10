bc81ae5ddb97a7c4682ec63e842b2ea94d3741f6
Carlos Becker <carlos@darkmatter.localdomain>
2025-06-10 12:12:31 -0300

    fix: git
    Signed-off-by: Carlos Becker <caarlos0@users.noreply.github.com>

--- init.fish
+++ init.fish
@@ -18,3 +18,4 @@ ln -sf $PWD/ghostty.config ~/.config/ghostty/config
 mkdir -p ~/.hammerspoon
 ln -sf $PWD/hammerspoon.lua /Users/carlos/.hammerspoon/init.lua
 ln -sf $PWD/nvim ~/.config/nvim
+ln -sf $PWD/gitconfig ~/.config/config
