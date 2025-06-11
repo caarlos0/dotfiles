ff01b48e100394a27fedf49abfdf4ff1bf5f9980
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-06-11 16:49:09 -0300

    renames

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-require("user.options")
-require("user.keymaps")
-require("user.autocommands")
+require("config.options")
+require("config.keymaps")
+require("config.autocommands")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -9,7 +9,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -24,5 +24,3 @@ require("lazy").setup({
   },
   checker = { enabled = true },
 })
-
--- require("auto-hlsearch").setup()
