4a3cd52aa07a76fc79c8dafacb3d75608caf2553
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-07-07 15:25:30 -0300

    feat: create caarlos0/debug docker image from this
    used with the `kdebug` alias

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,3 +4,5 @@ tag="$(date +%Y).$(expr $(date +%m) + 0).$(expr $(date +%d) + 0)"
 git tag -m "$tag" "$tag"
 git push --tags
 goreleaser release --clean
+docker build -t caarlos0/debug .
+docker push caarlos0/debug
