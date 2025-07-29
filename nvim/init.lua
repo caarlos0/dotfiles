b3955b97099721dfe51c74719683c744b2cea873
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-07-29 00:08:28 -0300

    chore(nvim): group plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,17 +3,32 @@ require("config.keymaps")
 require("config.autocommands")
 
 vim.pack.add({
-  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+  { src = "https://github.com/stevearc/dressing.nvim" },
+  { src = "https://github.com/rcarriga/nvim-notify" },
+  { src = "https://github.com/christoomey/vim-tmux-navigator" },
+  { src = "https://github.com/rgroli/other.nvim" },
+  { src = "https://github.com/danymat/neogen" },
+  { src = "https://github.com/asiryk/auto-hlsearch.nvim" },
+  { src = "https://github.com/famiu/bufdelete.nvim" },
+  { src = "https://github.com/norcalli/nvim-colorizer.lua" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  { src = "https://github.com/akinsho/git-conflict.nvim" },
 
+  -- lsp, formatting, and completions
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/stevearc/conform.nvim" },
   { src = "https://github.com/zbirenbaum/copilot.lua" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/giuxtaposition/blink-cmp-copilot" },
   {
     src = "https://github.com/saghen/blink.cmp",
     version = vim.version.range("1.*"),
   },
-  { src = "https://github.com/rafamadriz/friendly-snippets" },
-  { src = "https://github.com/giuxtaposition/blink-cmp-copilot" },
-  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
 
+  -- tpope gang
   { src = "https://github.com/tpope/vim-fugitive" },
   { src = "https://github.com/tpope/vim-rhubarb" },
   { src = "https://github.com/tpope/vim-abolish" },
@@ -22,23 +37,12 @@ vim.pack.add({
   { src = "https://github.com/tpope/vim-sleuth" },
   { src = "https://github.com/tpope/vim-speeddating" },
 
-  { src = "https://github.com/nvim-lualine/lualine.nvim" },
-  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
-  { src = "https://github.com/stevearc/dressing.nvim" },
-
+  -- telescope
   { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/nvim-telescope/telescope-github.nvim" },
-  {
-    src = "https://github.com/nvim-telescope/telescope.nvim",
-    -- version = vim.version.range("0.1.*"),
-  },
+  { src = "https://github.com/nvim-telescope/telescope.nvim" }, -- version = vim.version.range("0.1.*"),
 
-  { src = "https://github.com/christoomey/vim-tmux-navigator" },
-
-  { src = "https://github.com/rgroli/other.nvim" },
-  { src = "https://github.com/danymat/neogen" },
-  { src = "https://github.com/rcarriga/nvim-notify" },
-
+  -- treesitter and friends
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
   { src = "https://github.com/wansmer/treesj" },
@@ -50,13 +54,6 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/RRethy/nvim-treesitter-endwise" },
-
-  { src = "https://github.com/stevearc/conform.nvim" },
-  { src = "https://github.com/lewis6991/gitsigns.nvim" },
-  { src = "https://github.com/asiryk/auto-hlsearch.nvim" },
-  { src = "https://github.com/famiu/bufdelete.nvim" },
-  { src = "https://github.com/norcalli/nvim-colorizer.lua" },
-  { src = "https://github.com/akinsho/git-conflict.nvim" },
 })
 
 local opts = { noremap = true, silent = true }
@@ -357,3 +354,5 @@ vim.keymap.set("n", "<leader>q", ":bdelete!<cr>", {
   buffer = vim.api.nvim_get_current_buf(),
   desc = "Close buffer",
 })
+
+require("git-conflict").setup()
