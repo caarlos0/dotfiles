af75f8bf11c2094f41ddb6031282e4bb40851c79
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-07-28 21:25:07 -0300

    refactor(nvim): moved bdelete to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -350,3 +350,10 @@ require("blink.cmp").setup({
 })
 
 require("auto-hlsearch").setup()
+
+vim.keymap.set("n", "<leader>q", ":bdelete!<cr>", {
+  noremap = true,
+  silent = true,
+  buffer = vim.api.nvim_get_current_buf(),
+  desc = "Close buffer",
+})
