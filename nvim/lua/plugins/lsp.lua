1a7b5a862f702c76d4ab329e982560d664b19423
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-04 11:47:03 -0300

    fix(nvim): lua lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -195,6 +195,9 @@ lspconfig.lua_ls.setup({
       hint = {
         enable = true,
       },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
     },
   },
 })
