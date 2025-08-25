5cf74308479da16851e2793e2b32a7ae4a55e862
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-25 14:07:05 -0300

    feat(nvim): simplify telescope config
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,30 +1,9 @@
-local select_one_or_multi = function(prompt_bufnr)
-  local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-  local multi = picker:get_multi_selection()
-  if not vim.tbl_isempty(multi) then
-    require("telescope.actions").close(prompt_bufnr)
-    for _, j in pairs(multi) do
-      if j.path ~= nil then
-        vim.cmd(string.format("%s %s", "edit", j.path))
-      end
-    end
-  else
-    require("telescope.actions").select_default(prompt_bufnr)
-  end
-end
-
 local telescope = require("telescope")
 telescope.setup({
   defaults = {
     pickers = {
       find_files = {
         theme = "ivy",
-      },
-    },
-    mappings = {
-      i = {
-        ["<CR>"] = select_one_or_multi,
-        ["<C-y>"] = select_one_or_multi,
       },
     },
     prompt_prefix = "   ",
@@ -46,13 +25,12 @@ telescope.setup({
 })
 telescope.load_extension("gh")
 
-local opts = { noremap = true, silent = true }
-local builtin = require("telescope.builtin")
-
 local function ivy(opts)
   return require("telescope.themes").get_ivy(opts)
 end
 
+local builtin = require("telescope.builtin")
+local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-p>", function()
   builtin.find_files(ivy({
     find_command = {
@@ -66,12 +44,14 @@ vim.keymap.set("n", "<C-p>", function()
 end, opts)
 
 vim.keymap.set("n", "<leader>of", function()
-  builtin.oldfiles({
+  builtin.oldfiles(ivy({
     only_cwd = true,
-  })
+  }))
 end, opts)
 
-vim.keymap.set("n", "<leader>lg", require("config.telescope-multi"), opts)
+vim.keymap.set("n", "<leader>lg", function()
+  builtin.live_grep(ivy())
+end, opts)
 
 vim.keymap.set("n", "<leader>fb", function()
   builtin.buffers(ivy())
