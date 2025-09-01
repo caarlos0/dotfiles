7741eb1965ea98aa30432020366e7ae4785db132
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-31 22:09:30 -0300

    feat(tmux): remove tpm
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,3 @@ rm Brewfile && brew bundle dump
 
 echo "Updating Neovim..."
 nvim --headless "lua vim.pack.update()" +qa &>/dev/null
-
-echo "Updating tmux.."
-~/.config/tmux/plugins/tpm/bin/update_plugins all
