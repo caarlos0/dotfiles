d434e195cc33b72fac72c5580d86372cc5d53a8b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-10-23 10:32:22 -0300

    fix(nvim): disable treesitter inside documentation popups
    it seems to not work most of the time.
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -417,7 +417,7 @@ require("blink.cmp").setup({
     documentation = {
       auto_show = true,
       auto_show_delay_ms = 250,
-      treesitter_highlighting = true,
+      treesitter_highlighting = false,
     },
     menu = {
       draw = {
