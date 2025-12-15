d5878255a0f8225b819d448cc6de15ba8a897b72
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-12-15 09:02:39 -0300

    fix: commit Brewfile and nvim-pack-lock.json
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,3 +11,11 @@ rm Brewfile && brew bundle dump --no-go
 
 echo "Updating Neovim..."
 nvim --headless +"lua vim.pack.update()" +qa &>/dev/null
+
+if [ "$(git symbolic-ref --short HEAD)" = "main" ] && [ "$(git status --porcelain=2 | wc -l)" = "1" ]; then
+  echo "Commiting changes..."
+  git add Brewfile
+  git commit -m 'chore(darwin): update Brewfile'
+  git add ./nvim/nvim-pack-lock.json
+  git commit -m 'chore(nvim): update lockfile'
+fi
