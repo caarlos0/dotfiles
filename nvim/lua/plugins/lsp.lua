9f46b3316918d7f3779c960060b102c9048e6b63
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-01-08 16:04:47 -0300

    fix(nvim): faster lua_ls
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -190,10 +190,6 @@ vim.lsp.config("yamlls", {
 })
 vim.lsp.enable("yamlls")
 
--- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ";", {})
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
 vim.lsp.config("lua_ls", {
   capabilities = capabilities,
   on_attach = on_attach,
@@ -207,7 +203,10 @@ vim.lsp.config("lua_ls", {
         enable = true,
       },
       workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
       },
     },
   },
