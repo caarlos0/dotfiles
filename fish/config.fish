d55a2040ff29869c9a15ffd75e3786ee28fc2c67
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-01-14 14:06:26 -0300

    reafactor(fish): move prompt to fish/functions
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- fish/config.fish
+++ fish/config.fish
@@ -86,51 +86,6 @@ status is-interactive; and begin
     set fish_greeting
     fish_config theme choose vesper
 
-    # Based on astronaut, but only shows user@host when connected via SSH,
-    # uses '$' as prompt symbol instead of '❯', and supports transient prompts.
-    #
-    # See: https://github.com/fish-shell/fish-shell/blob/master/share/prompts/astronaut.fish
-    function fish_prompt
-        set -l last_status $status
-        set -l normal (set_color normal)
-        set -l status_color (set_color brgreen)
-        set -l cwd_color (set_color $fish_color_cwd)
-        set -l vcs_color (set_color brpurple)
-        set -l user_info ""
-        set -l prompt_status ""
-
-        # Since we display the prompt on a new line allow the directory names to be longer.
-        set -q fish_prompt_pwd_dir_length
-        or set -lx fish_prompt_pwd_dir_length 0
-
-        # Color the prompt differently when we're root
-        set -l suffix '$'
-        if functions -q fish_is_root_user; and fish_is_root_user
-            if set -q fish_color_cwd_root
-                set cwd_color (set_color $fish_color_cwd_root)
-            end
-            set suffix '#'
-        end
-
-        # Color the prompt in red on error
-        if test $last_status -ne 0
-            set status_color (set_color $fish_color_error)
-            set prompt_status $status_color "[" $last_status "]" $normal
-        end
-
-        if test -n "$SSH_CLIENT"
-            set user_info (prompt_login) ' '
-        end
-
-        # if its final rendering, only show the suffix
-        if contains -- --final-rendering $argv
-            echo -n -s $status_color $suffix ' ' $normal
-        else
-            echo -s $user_info $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
-            echo -n -s $status_color $suffix ' ' $normal
-        end
-    end
-
     # enable transient prompt to declutter the terminal
     set -g fish_transient_prompt 1
 
