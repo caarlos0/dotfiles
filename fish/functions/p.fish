5f79ac96aa49f20670408d0918b2400c728700d3
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-02-04 10:49:13 -0300

    fix(fish): improve p.fish
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- fish/functions/p.fish
+++ fish/functions/p.fish
@@ -1,6 +1,8 @@
 function p --description "Switch to a project directory"
     set -l query $argv[1]
-    set -l selected (
+    set -l ratio_threshold 3
+
+    set -l results (
         find "$PROJECTS" -maxdepth 1 -type d |
         while read dir
             zoxide query -l -s "$dir/"
@@ -9,10 +11,26 @@ function p --description "Switch to a project directory"
         grep -v "$PROJECTS" |
         grep / |
         sort -rnk1 |
-        uniq |
-        awk '{print $2}' |
-        fzf --no-sort --select-1 --query="$query" --prompt="  "
+        uniq
     )
+    test -z "$results" && return 0
+
+    if test -n "$query"
+        set results (printf '%s\n' $results | fzf --no-sort --filter="$query")
+        set -l scores (printf '%s\n' $results | awk '{print $1}')
+        if test (count $scores) -ge 1
+            set -l top $scores[1]
+            set -l second (test (count $scores) -ge 2 && echo $scores[2] || echo 0)
+            if test $second -eq 0 -o (math "$top / $second") -ge $ratio_threshold
+                set selected (echo $results[1] | awk '{print $2}')
+            end
+        end
+    end
+
+    if test -z "$selected"
+        set selected (printf '%s\n' $results | awk '{print $2}' | fzf --no-sort --select-1 --query="$query" --prompt="  ")
+    end
+
     test -z "$selected" && return 0
     zoxide add "$PROJECTS/$selected"
     cd "$PROJECTS/$selected"
