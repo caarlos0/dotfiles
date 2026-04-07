ea4ccdf2201904adf8d8829b304b9f0e6bcaa6bf
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-04-07 15:45:32 -0300

    feat(nvim): ui2
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -215,6 +215,55 @@ end
 --- UI
 ---
 
+-- Experimental UI2: floating cmdline and messages
+vim.o.cmdheight = 0
+require("vim._core.ui2").enable({
+  enable = true,
+  msg = {
+    targets = {
+      [""] = "msg",
+      empty = "cmd",
+      bufwrite = "msg",
+      confirm = "cmd",
+      emsg = "pager",
+      echo = "msg",
+      echomsg = "msg",
+      echoerr = "pager",
+      completion = "cmd",
+      list_cmd = "pager",
+      lua_error = "pager",
+      lua_print = "msg",
+      progress = "pager",
+      rpc_error = "pager",
+      quickfix = "msg",
+      search_cmd = "cmd",
+      search_count = "cmd",
+      shell_cmd = "pager",
+      shell_err = "pager",
+      shell_out = "pager",
+      shell_ret = "msg",
+      undo = "msg",
+      verbose = "pager",
+      wildlist = "cmd",
+      wmsg = "msg",
+      typed_cmd = "cmd",
+    },
+    cmd = {
+      height = 0.5,
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.3,
+      timeout = 5000,
+    },
+    pager = {
+      height = 0.5,
+    },
+  },
+})
+
 require("catppuccin").setup({
   flavour = "mocha",
   color_overrides = {
