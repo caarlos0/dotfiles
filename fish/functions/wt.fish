d2de9d469eba5eae5c92a39a6cde3fde2e70c36d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-05-08 09:39:49 -0300

    fix(git): wt use branch name to list and switch
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- fish/functions/wt.fish
+++ fish/functions/wt.fish
@@ -1,19 +1,22 @@
 function __wt_complete
     git rev-parse --git-dir >/dev/null 2>&1
     or return
-    set -l remote_url (git remote get-url origin 2>/dev/null)
-    set -l prefix
-    if test -n "$remote_url"
-        set prefix (basename $remote_url .git)-
-    else
-        set prefix (basename (git rev-parse --show-toplevel))-
-    end
-    git worktree list --porcelain | grep '^worktree ' | string replace 'worktree ' '' | while read -l wt
-        string replace -- $prefix '' (basename $wt)
+    set -l main_wt (git worktree list --porcelain | head -1 | string replace 'worktree ' '')
+    set -l wt_path ""
+    for line in (git worktree list --porcelain)
+        if string match -q 'worktree *' $line
+            set wt_path (string replace 'worktree ' '' $line)
+        else if string match -q 'branch *' $line
+            if test "$wt_path" != "$main_wt"
+                string replace 'branch refs/heads/' '' $line
+            end
+            set wt_path ""
+        end
     end
 end
 
 complete --command wt --no-files
 complete --command wt --long delete --short d --description "Remove a worktree"
 complete --command wt --long list --short l --description "List worktrees"
+complete --command wt --long cleanup --short c --description "Remove all merged worktrees"
 complete --command wt --arguments '(__wt_complete)' --description "Worktree"
