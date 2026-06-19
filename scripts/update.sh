1ff299885f58dd953c8d964f6091278d1133ab13
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-06-19 00:39:47 -0300

    fix(system): update.sh
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,7 +8,7 @@ brew autoremove
 brew cleanup
 
 echo "Updating Brewfile..."
-rm Brewfile && brew bundle dump --no-go --no-uv --no-cargo
+brew bundle dump --no-go --no-uv --no-cargo --force
 
 echo "Updating Neovim plugins..."
 nvim --headless +"lua vim.pack.update(nil, { force = true })" +qa &>/dev/null
@@ -16,7 +16,7 @@ nvim --headless +"lua vim.pack.update(nil, { force = true })" +qa &>/dev/null
 if [ "$(git symbolic-ref --short HEAD)" = "main" ]; then
   echo "Commiting changes..."
   git add Brewfile
-  git commit -m 'chore(darwin): update Brewfile'
+  git diff --cached --quiet || git commit -m 'chore(darwin): update Brewfile'
   git add ./nvim/nvim-pack-lock.json
-  git commit -m 'chore(nvim): update lockfile'
+  git diff --cached --quiet || git commit -m 'chore(nvim): update lockfile'
 fi
